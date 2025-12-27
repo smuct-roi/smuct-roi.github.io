d1502f765c701ae025e3fa3110ca14f9ba615231
Fahad <nazmulhaquefahad3@gmail.com>
2025-12-27 21:35:57 +0600

    config changed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smuct-roi.github.io/'
+  base: '/'  // ✅ correct for user page
 })
